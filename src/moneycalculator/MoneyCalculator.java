ea378fe09361cc7e0ae20e499869fff344e881fe
aleja <aleja@LAPTOP-4FFNEU4Q>
2023-01-22 23:57:13 +0000

    MoneyCalculator

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -6,7 +6,6 @@ import persistence.CurrencyLoader;
 import persistence.ExRateLoader;
 
 public class MoneyCalculator {
-
     public static void main(String[] args) {
         MoneyApp moneyApp = MoneyApp.start();
     }
